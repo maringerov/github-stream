bd0a4d1d5267c46ce91ad88a3a6fdaf687d61694
Marin Gerov <marin.gerov@gmail.com>
2015-08-07 22:35:35 +0200

    Implemented dynamic link fetching for repo links for each event

--- app/components/Event.js
+++ app/components/Event.js
@@ -3,14 +3,34 @@ import Radium from 'radium';
 
 @Radium
 export default class Event extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repo_url: ''
+    };
+  }
+
   static propTypes = {
     event: PropTypes.object.isRequired
   }
+
+  componentWillMount() {
+    fetch(this.props.event.repo.url)
+      .then(res => {
+        return res.json()
+      })
+      .then(json => {
+        this.setState({
+          repo_url: json.html_url
+        });
+      });
+  }
+  
   render() {
     const { event } = this.props;
     return (
       <li style={styles.event}>
-        {event.type} on {event.repo.name}
+        {event.type} on <a href={this.state.repo_url}>{event.repo.name}</a>
         <br />
         <small>{event.created_at}</small>
       </li>
